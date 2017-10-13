e1a9d4820acc45a2c87b3965e455dc8465bd4660
Anton-Leonov-IKBO-03-17 <oh22@mail.ru>
2017-10-13 16:41:39 +0300

    Create demo work.c

---
+++ demo work.c
@@ -0,0 +1,81 @@
+#define NMAX 100
+struct stack {
+  float elem[NMAX];
+  int top;
+};
+
+void init(struct stack *stk) {
+  stk->top = 0;
+}
+
+void push(struct stack *stk, float f) {
+  if(stk->top < NMAX) {
+    stk->elem[stk->top] = f;
+    stk->top++;
+  } else
+    printf("Стек полон, количество элементов: %d !\n", stk->top);
+}
+
+float pop(struct stack *stk) {
+  float elem;
+  if((stk->top) > 0) {
+    stk->top--;
+    elem = stk->elem[stk->top];
+    return(elem);
+  } else {
+    printf("Стек пуст!\n");
+    return(0);
+  }
+}
+
+float stkTop(struct stack *stk) {
+  if((stk->top) > 0) {
+    return( stk->elem[stk->top-1]);
+  } else {
+    printf("Стек пуст!\n");
+    return(0);
+  }
+}
+
+int gettop(struct stack *stk) {
+return(stk->top);}
+ 
+int isempty(struct stack *stk) {
+  if((stk->top) == 0)    return(1);
+  else return(0);
+}
+
+void stkPrint(struct stack *stk) {
+  int i;
+  i=stk->top;
+  if(isempty(stk)==1) return;
+  do {
+    i--;
+    printf("%f\n", stk->elem[i]);
+  } while(i>0);
+}
+
+int main() {
+  struct stack *stk;
+  int i,n;
+  float elem;
+  stk = (struct stack*)malloc(sizeof(struct stack));
+  init(stk);
+  printf("Введите количество элементов в стеке: ");
+  scanf("%d", &n);
+  for(i=0;i<n;i++) {
+    printf("Введите элемент %d:", i);
+    scanf("%f", &elem);
+    push(stk,elem);
+  }
+  printf("В стеке %d элементов\n", gettop(stk));
+  printf("\n");
+  stkPrint(stk);
+  printf("Верхний элемент %f\n",stkTop(stk));
+  do {
+    printf("Извлекаем элемент %f, ", pop(stk));
+    printf("в стеке осталось %d элементов\n", gettop(stk));
+  } while(isempty(stk)==0);
+  getchar(); getchar();
+  return 0;
+}
